2d9f95d2720c1990fd117769e88c0b461ad66193
Michelle Ruiter <mruiter4@gmail.com>
2020-05-08 11:55:27 -0700

    Refactored MelonOrder Classes

--- melons.py
+++ melons.py
@@ -1,35 +1,6 @@
 """Classes for melon orders."""
-
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
-
-    def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder():
-    """An international (non-US) melon order."""
-
+class AbstractMelonOrder:
+    """An abstract base Melon class"""
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
@@ -37,13 +8,15 @@ class InternationalMelonOrder():
         self.qty = qty
         self.country_code = country_code
         self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
+
 
     def get_total(self):
         """Calculate price, including tax."""
 
         base_price = 5
+        if self.species == 'christmas melon':
+            base_price*=1.5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -52,6 +25,22 @@ class InternationalMelonOrder():
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = 0.08
+    country_code = 'USA'
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty, 'USA')
+        """Initialize melon order attributes."""
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    order_type = "international"
+    tax = 0.17
 
     def get_country_code(self):
         """Return the country code."""
